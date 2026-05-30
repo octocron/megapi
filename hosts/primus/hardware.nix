67eb02cd7d3ba010494af11c78921577ad55b69c
megacron <megacron@d3c3p7.com>
2026-05-29 23:51:14 -0400

    added nixpkgs-rpi to see if it helps

--- hosts/primus/hardware.nix
+++ hosts/primus/hardware.nix
@@ -1,7 +1,8 @@
 { inputs, ... }:
 {
-  imports = [
-    inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.base
-    inputs.nixos-raspberrypi.nixosModules.raspberry-pi-5.page-size-16k
+  imports = with inputs.nixos-raspberrypi.nixosModules; [
+    raspberry-pi-5.base
+    raspberry-pi-5.page-size-16k
+    nixpkgs-rpi
   ];
 }
