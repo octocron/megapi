cfc35cceafc58d5b03265d69f45bbe7a44657701
megacron <megacron@d3c3p7.com>
2026-06-01 22:07:30 -0400

    nixpkgs-rpi path corrected

--- hosts/primus/hardware.nix
+++ hosts/primus/hardware.nix
@@ -1,11 +1,11 @@
 { inputs, ... }:
 {
   imports = with inputs.nixos-raspberrypi; [
-    nixpkgs-rpi
     lib.inject-overlays
     nixosModules.raspberry-pi-5.base
     nixosModules.raspberry-pi-5.bluetooth
     nixosModules.raspberry-pi-5.display-vc4
     nixosModules.raspberry-pi-5.page-size-16k
+    nixosModules.nixpkgs-rpi
   ];
 }
