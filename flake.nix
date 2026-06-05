03f2ff7486cadbbd38ec7974e7cfe9e23331f96b
megacron <megacron@d3c3p7.com>
2026-06-05 17:04:52 -0400

    locking flake to 26.05

--- flake.nix
+++ flake.nix
@@ -18,13 +18,13 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     megavim.url = "gitlab:megacron/megavim?ref=nixvim";
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
 
     nixos-anywhere.url = "github:nix-community/nixos-anywhere";
     nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
