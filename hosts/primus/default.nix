314a0cb871d75898a324b60581fb65d66e8bb6a5
megacron <megacron@d3c3p7.com>
2026-06-19 20:12:05 -0400

    changed import order

--- hosts/primus/default.nix
+++ hosts/primus/default.nix
@@ -2,10 +2,10 @@ _: {
   # INFO: NixOS Headless Raspberry Pi5 using NVME
   system.stateVersion = "26.05";
   imports = [
+    ./hardware.nix
+    ./disko.nix
     ../../boot/pi5.nix
     ../../core
     ../../users/megacron.nix
-    ./disko.nix
-    ./hardware.nix
   ];
 }
